19a17ca945a4edc35e53ac9250ae5f5e41bf30da
Carol Cordova <carolcordova7@gmail.com>
2017-03-05 15:16:09 -0700

    correct js file

---
+++ script.js
@@ -0,0 +1,52 @@
+//Create a sorting bin app that allows users to enter names. If the name starts //with “ A “ , it goes into list one.  If the name has the word “Alex” anywhere //within the name, it goes in list “B”.  If neither are true, it goes in list //“  C “ . (edited)*/
+
+$(document).ready(function() {
+
+   function checkName(str)  {
+      var input_name = str.toLowerCase();
+        var result = /^alex/.test(input_name);
+        if (result=== true){
+             $("#alexList").append("<div>"+input_name+"</div>");
+        } else
+         if (input_name[0] === "a") {
+            $("#aList").append("<div>"+input_name+"</div>");
+         } else {
+            $("#nList").append("<div>"+input_name+"</div>");
+          }
+       }
+    })
+
+
+
+
+// THIS CODE TEST THAT LOGIC IS CORRECT  IT DOES WORK
+//    function checkName(str)  {
+//        var input_name = str.toLowerCase();
+//          var result = /^alex/.test(input_name);
+//          if (result=== true){
+//             console.log("alex is the name");
+//          } else
+//       if (input_name[0] === "a") {
+//            console.log("this is for A List")
+//         } else {
+//            console.log ("neither");
+//            }
+//         }
+//checkName("Alex");
+
+
+
+
+
+//           if (input_name.search(/alex/ig) >= 0){
+//             alexList.append("input_name");
+//             $("#alexList").append("<div>"+str+"</div>");
+//           } else
+//           if (input_name.search(/a/ig) >= 0) {
+//             aList.append("input_name");
+//             $("#aList").append("<div>"+str+"</div>");
+//          } else {
+//             nList.append("input_name");
+//              $("#nList").append("<div>"+str+"</div>");
+   //       }
+//        }
